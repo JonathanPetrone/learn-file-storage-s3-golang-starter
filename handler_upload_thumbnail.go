033132d1fc8040828f3736a2b6946f9f2102dc79
Jonathan Petrone <44399523+JonathanPetrone@users.noreply.github.com>
2025-01-30 11:06:39 +0100

    ch1 l6

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse multipart form", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	base64str := base64.StdEncoding.EncodeToString(imageData)
+	dataUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, base64str)
+
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "video not found", err)
+		return
+	}
+
+	if videoMetaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "user is not video owner", err)
+		return
+	}
+
+	videoMetaData.ThumbnailURL = &dataUrl
+
+	err = cfg.db.UpdateVideo(videoMetaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't update video", err)
+		return
+	}
+
+	// Add debug logging here
+	fmt.Printf("After update - Video ID: %s\n", videoMetaData.ID)
+	fmt.Printf("Thumbnail URL: %v\n", *videoMetaData.ThumbnailURL)
+	fmt.Printf("Updated video metadata: %+v\n", videoMetaData)
+
+	respondWithJSON(w, http.StatusOK, videoMetaData)
 }
